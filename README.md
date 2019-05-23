f2719d19c8696cfc42472868a2fc56fa3536a025
onnion <marcio.hsdl@gmail.com>
2019-05-23 15:48:09 -0300

    [master] | att lib and readme

--- README.md
+++ README.md
@@ -1,32 +1,36 @@
-# whatsapp firefox fix
+# Whatsapp api fix
 ## This repository implement solution for firefox whatsapp break link 
 
 ### Hot to use
 
-1. `Import script`
-
-```
-<script src="PATH_TO_SCRIPT"></script>
-```
-
-2. `Use funcion only number`
+1. Use funcion only number
 ```javascript
-// ...
-const redirect_whatsapp_link = generate(123456789);
-// ...
+var whatsappFix = require('whatsapp-api-fix');
+
+var params = {number: 123456789};
+var redirect_whatsapp_link = whatsappFix.generate(params);
 
 // > http://web.whatsapp.com/send?phone=123456789&text=
 // > http://api.whatsapp.com/send?phone=123456789&text=
 ```
 
-
-
-3. `Use funcion number and text`
+2. Use funcion number and text
 ```javascript
-// ...
-const redirect_whatsapp_link = generate(123456789, 'text');
-// ...
+var whatsappFix = require('whatsapp-api-fix');
+
+var params = {number: 123456789, text: 'text'};
+var redirect_whatsapp_link = whatsappFix.generate(params);
 
 // > http://web.whatsapp.com/send?phone=123456789&text=text
 // > http://api.whatsapp.com/send?phone=123456789&text=text
+```
+
+3. Use funcion to search in `stackoverflow` in case of error
+    ##### IN THIS CASE, A NEW TAB IS OPEN IN STACKOVERFLOW
+
+```javascript
+var whatsappFix = require('whatsapp-api-fix');
+
+var params = {number: 123456789, text: 'text', search: true};
+var redirect_whatsapp_link = whatsappFix.generate(params);
 ```
--- whatsapp.js
+++ whatsapp.js
@@ -1,17 +1,44 @@
 /**
+ * Function to search in
+ * 
+ * @param {Error} error 
+ */
+function searchStackOverflow(error) {
+    var $window = window.open('https://stackoverflow.com/search?q=' + error.message, '_blank');
+    $window.focus();
+}
+
+/**
  * Function to get specific link for redirect whatsapp
  * 
- * @param {string | number} number 
- * @param {string} text 
- * @return {string}
+ * @param {{search?: boolean, number: string | number, text?: string}} params
+ * @return {string} urlRedirect
  */
-function getRedirect(number, text = '') {
-    var $userAgent = navigator.userAgent;
-    var $isMobile = $userAgent.includes('iPhone') || $userAgent.includes('Android') || $userAgent.includes('webOS') || $userAgent.includes('BlackBerry') || userAgent.includes('iPod');
+function getRedirect(params) {
+    var urlRedirect = '';
 
-    return `http://${$isMobile ? 'api' : 'web' }.whatsapp.com/send?phone=${number}&text=${text}`;
+    try {
+        var $navigator = navigator;
+        var $userAgent = $navigator.userAgent;
+        var $isMobile = $userAgent.includes('iPhone') || $userAgent.includes('Android') || $userAgent.includes('webOS') || $userAgent.includes('BlackBerry') || userAgent.includes('iPod');
+        var $prefix = 'web';
+
+        if ($isMobile) { $prefix = 'api'; }
+
+        if (params.hasOwnProperty('text')) {
+            urlRedirect = "http://" + $prefix + ".whatsapp.com/send?phone=" + params.number + "&text=" + params.text;
+        } else {
+            urlRedirect = "http://" + $prefix + ".whatsapp.com/send?phone=" + params.number + "&text=";
+        }
+
+    } catch (error) {
+        console.error(error.message);
+        if (params.hasOwnProperty('search')) {
+            searchStackOverflow(error);
+        }
+    }
+
+    return urlRedirect;
 }
 
-module.exports = {
-    getRedirect: getRedirect
-}
+module.exports = { getRedirect: getRedirect }
